f15fe70a15fa7d318e43bb503f1a0c66c1038aac
Stephen Bolton <stephen@bitsonthemind.com>
2023-07-08 18:09:54 -0400

    Improve dunst styling

--- home/desktop/dunst.nix
+++ home/desktop/dunst.nix
@@ -1,12 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  home.packages = [ pkgs.inter ];
+
   services.dunst = {
     enable = true;
     settings = {
       global = {
-        frame_color = "#2D2B40";
-        geometry = "350x200-18+20";
+        font = "Inter 12";
+        frame_color = "#100E23";
+        frame_width = "2";
+        origin = "top-center";
+        width = "300";
+        height = "200";
         padding = 16;
         horizontal_padding = 16;
         separator_color = "#585273";
