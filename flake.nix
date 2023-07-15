974d3cfad620d332994941749af5a75954df87a4
Stephen Bolton <stephen@bitsonthemind.com>
2023-07-15 18:30:55 -0400

    Add host config for neo

--- flake.nix
+++ flake.nix
@@ -92,6 +92,45 @@
           }
         ];
       };
+      neo = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        # The Nix module system can modularize configuration, improving the maintainability of configuration.
+        #
+        # Each parameter in the `modules` is a Nix Module, and there is a partial introduction to it in the nixpkgs manual:
+        #    <https://nixos.org/manual/nixpkgs/unstable/#module-system-introduction>
+        # It is said to be partial because the documentation is not complete, only some simple introductions
+        #    (such is the current state of Nix documentation...)
+        # A Nix Module can be an attribute set, or a function that returns an attribute set.
+        # If a Module is a function, this function can only have the following parameters:
+        #
+        #  lib:     the nixpkgs function library, which provides many useful functions for operating Nix expressions
+        #            https://nixos.org/manual/nixpkgs/stable/#id-1.4
+        #  config:  all config options of the current flake
+        #  options: all options defined in all NixOS Modules in the current flake
+        #  pkgs:   a collection of all packages defined in nixpkgs.
+        #           you can assume its default value is `nixpkgs.legacyPackages."${system}"` for now.
+        #           can be customed by `nixpkgs.pkgs` option
+        #  modulesPath: the default path of nixpkgs's builtin modules folder,
+        #               used to import some extra modules from nixpkgs.
+        #               this parameter is rarely used, you can ignore it for now.
+        #
+        # Only these parameters can be passed by default.
+        # If you need to pass other parameters, you must use `specialArgs` by uncomment the following line
+        # specialArgs = {...}  # pass custom arguments into sub module.
+        modules = [
+          ./hosts/neo
+          hyprland.nixosModules.default
+          { programs.hyprland.enable = true; }
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.users.orlando = import ./home;
+          }
+        ];
+      };
     };
   };
 }
