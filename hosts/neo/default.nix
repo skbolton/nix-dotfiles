90f90e35b46dafeb3d5e9dc3a8b6fdf0dc08cfd0
Stephen Bolton <stephen@bitsonthemind.com>
2023-07-21 20:19:42 -0400

    Use iwd for networkmanager wifi backend
    iwd is miles better than wpa_supplicant

--- hosts/neo/default.nix
+++ hosts/neo/default.nix
@@ -30,6 +30,7 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.wifi.backend = "iwd";
 
   # Set your time zone.
   time.timeZone = "America/New_York";
