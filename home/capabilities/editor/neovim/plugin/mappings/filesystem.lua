31f98c319747a5fbf4635132ffc583f2385ed1e2
Stephen Bolton <stephen@bitsonthemind.com>
2023-07-30 13:50:40 -0400

    Turn off nnn default keybinds
    I was triggering some weird keybinds that were messing up my filesystem
    and I wasn't realizing it.

--- home/capabilities/editor/neovim/plugin/mappings/filesystem.lua
+++ home/capabilities/editor/neovim/plugin/mappings/filesystem.lua
@@ -6,6 +6,8 @@ vim.g["nnn#action"] = {
   ['<c-v>'] = 'vsplit' 
 }
 
+vim.g["nnn#set_default_mappings"] = false
+
 map('n', '<leader>E', '<CMD>NnnExplorer<CR>')
 map('n', '<leader>e', '<CMD>NnnPicker %:p<CR>')
 map('n', '<leader>_', ':silent grep ', { silent = false })
