2053307dfecfdb0358e56c2c6d02a1961f7999b6
Stephen Bolton <stephen@bitsonthemind.com>
2023-07-30 13:49:32 -0400

    Tweak fzf theme to better handle light themes
    More work needs to happen here to possibly toggle theme in light vs dark
    states.

--- home/capabilities/shell.nix
+++ home/capabilities/shell.nix
@@ -135,13 +135,13 @@
     enableZshIntegration = true;
     colors = {
       "bg+" = "-1";
-      "fg" = "15";
+      "fg" = "-1";
       "fg+" = "-1";
       "prompt" = "6";
       "header" = "5";
       "pointer" = "2";
-      "hl" = "3";
-      "hl+" = "3";
+      "hl" = "2";
+      "hl+" = "2";
       "spinner" = "05";
       "info" = "15";
       "border" = "15";
