c012de584ca70665ad763d9d4ef6f84db33e8fc9
Stephen Bolton <stephen@bitsonthemind.com>
2023-09-02 12:07:26 -0400

    Update to linuxmobile new hyprland animations

--- home/capabilities/desktop/hyprland/config.nix
+++ home/capabilities/desktop/hyprland/config.nix
@@ -97,16 +97,21 @@ env = GDK_SCALE,2
 env = XCURSOR_SIZE,24
 
 animations {
-    enabled = true
+  enabled = true
 
-    # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+  bezier = wind, 0.05, 0.9, 0.1, 1.05
+  bezier = winIn, 0.1, 1.1, 0.1, 1.1
+  bezier = winOut, 0.3, -0.3, 0, 1
+  bezier = liner, 1, 1, 1, 1
 
-    bezier = overshot, 0.13, 0.99, 0.29, 1.1
-    animation = windows, 1, 4, overshot, slide
-    animation = windowsOut, 1, 5, default, popin 80%
-    animation = border, 1, 5, default
-    animation = fade, 1, 8, default
-    animation = workspaces, 1, 6, overshot, slidevert
+  animation = windows, 1, 6, wind, slide
+  animation = windowsIn, 1, 6, winIn, slide
+  animation = windowsOut, 1, 5, winOut, slide
+  animation = windowsMove, 1, 5, wind, slide
+  animation = border, 1, 1, liner
+  animation = borderangle, 1, 30, liner, loop
+  animation = fade, 1, 10, default
+  animation = workspaces, 1, 5, wind
 }
 
 dwindle {
