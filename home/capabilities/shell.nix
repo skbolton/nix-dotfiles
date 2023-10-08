9d0b3210ae74eaec0ceb538c8e07381ffa1fa646
Stephen Bolton <stephen@bitsonthemind.com>
2023-10-08 17:41:29 -0400

    Add cursor mode back
    This was in titan and would switch between beam and block cursor when
    going in and out of vim normal/insert mode.

--- home/capabilities/shell.nix
+++ home/capabilities/shell.nix
@@ -68,6 +68,33 @@
     setopt EXTENDED_GLOB        # Use extended globbing syntax
     '';
     initExtra = ''
+    cursor_mode() {
+      # See https://ttssh2.osdn.jp/manual/4/en/usage/tips/vim.html for cursors
+      cursor_block='\e[2 q'
+      cursor_beam='\e[5 q'
+
+      function zle-keymap-select {
+          if [[ $${KEYMAP} == vicmd ]] ||
+              [[ $1 = 'block' ]]; then
+              echo -ne $cursor_block
+          elif [[ $${KEYMAP} == main ]] ||
+              [[ $${KEYMAP} == viins ]] ||
+              [[ $${KEYMAP} = \'\' ]] ||
+              [[ $1 = 'beam' ]]; then
+              echo -ne $cursor_beam
+          fi
+      }
+
+      zle-line-init() {
+          echo -ne $cursor_beam
+      }
+
+      zle -N zle-keymap-select
+      zle -N zle-line-init
+    }
+
+    cursor_mode
+
     color=$(( ( RANDOM % 6 ) + 1 ))
     tput setaf $color && ${pkgs.toilet}/bin/toilet -F border -t -f pagga "Bit by Bit"
     '';
