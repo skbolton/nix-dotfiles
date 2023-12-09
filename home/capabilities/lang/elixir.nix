1409b5540c81d015e845fc9491eed16c0840ed55
Stephen Bolton <stephen@bitsonthemind.com>
2023-12-09 15:10:37 -0500

    Port iex shell prompt from titan

--- home/capabilities/lang/elixir.nix
+++ home/capabilities/lang/elixir.nix
@@ -5,6 +5,15 @@
     ERL_AFLAGS = "-kernel shell_history enabled";
   };
 
+  home.file.".iex.exs".text = ''
+  IEx.configure(
+          default_prompt:
+            "#{IO.ANSI.magenta} #{IO.ANSI.reset}(%counter) |",
+          continuation_prompt:
+            "#{IO.ANSI.magenta} #{IO.ANSI.reset}(.) |"
+        )
+  '';
+
   home.packages = with pkgs; [
     elixir_1_15
     erlang_26
