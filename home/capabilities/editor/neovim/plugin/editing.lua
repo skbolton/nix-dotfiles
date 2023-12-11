c5dab5c7a4e79927eee2712237493424e2c09892
Stephen Bolton <stephen@bitsonthemind.com>
2023-12-11 15:05:16 -0500

    Tune notify and noice to be able to include some info messages
    Added some filters to get rid of the really annoying info messages that
    were popping up too much.

--- home/capabilities/editor/neovim/plugin/editing.lua
+++ home/capabilities/editor/neovim/plugin/editing.lua
@@ -1,4 +1,7 @@
 local api = vim.api
+local notify = require 'notify'
+
+vim.notify = notify
 
 vim.g.mkdp_theme = 'light'
 
@@ -17,10 +20,10 @@ require 'colorizer'.setup({
   }
 })
 
-require 'notify'.setup {
+notify.setup {
   background_colour = "#1e1c31",
   timeout = 2500,
-  level = vim.log.levels.WARN,
+  level = vim.log.levels.INFO,
   render = "compact",
   stages = "fade_in_slide_out"
 }
@@ -48,7 +51,26 @@ require 'noice'.setup {
     long_message_to_split = true, -- long messages will be sent to a split
     inc_rename = false, -- enables an input dialog for inc-rename.nvim
     lsp_doc_border = false, -- add a border to hover docs and signature help
-  }
+  },
+  routes = {
+    {
+      view = "notify",
+      filter = { event = "msg_showmode" },
+    },
+    {
+      filter = {
+        event = 'msg_show',
+        any = {
+          { find = '%d+L, %d+B' },
+          { find = '; after #%d+' },
+          { find = '; before #%d+' },
+          { find = '%d fewer lines' },
+          { find = '%d more lines' },
+        },
+      },
+      opts = { skip = true },
+    }
+  },
   views = {
     cmdline_popup = {
       position = {
