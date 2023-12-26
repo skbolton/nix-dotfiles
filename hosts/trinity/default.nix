3e8ba019b57adba48480626c13fd4648851c5b81
Stephen Bolton <stephen@bitsonthemind.com>
2023-12-25 19:57:24 -0500

    Add open tablet driver to trinity

--- hosts/trinity/default.nix
+++ hosts/trinity/default.nix
@@ -163,6 +163,11 @@
     enable = true;
   };
 
+  hardware.opentabletdriver = {
+    enable = true;
+    daemon.enable = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
