124149a68dd63d59b946ef69a2d69cfa69be6e9f
Stephen Bolton <stephen@bitsonthemind.com>
2024-01-09 18:30:21 -0500

    Run formatter on flake.nix

--- flake.nix
+++ flake.nix
@@ -51,109 +51,110 @@
   # parameters in `outputs` are defined in `inputs` and can be referenced by their names.
   # However, `self` is an exception, This special parameter points to the `outputs` itself (self-reference)
   # The `@` syntax here is used to alias the attribute set of the inputs's parameter, making it convenient to use inside the function.
-  outputs = { self, nixpkgs, home-manager, NixOS-WSL, ... }@inputs: 
+  outputs = { self, nixpkgs, home-manager, NixOS-WSL, ... }@inputs:
     let
       inherit (self) outputs;
-    in {
-    nixosModules = import ./modules/nixos;
-    nixosConfigurations = {
-      # By default, NixOS will try to refer the nixosConfiguration with its hostname.
-      # so the system named `nixos-test` will use this configuration.
-      # However, the configuration name can also be specified using `sudo nixos-rebuild switch --flake /path/to/flakes/directory#<name>`.
-      # The `nixpkgs.lib.nixosSystem` function is used to build this configuration, the following attribute set is its parameter.
-      # Run `sudo nixos-rebuild switch --flake .#nixos-test` in the flake's directory to deploy this configuration on any NixOS system
-      trinity = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+    in
+    {
+      nixosModules = import ./modules/nixos;
+      nixosConfigurations = {
+        # By default, NixOS will try to refer the nixosConfiguration with its hostname.
+        # so the system named `nixos-test` will use this configuration.
+        # However, the configuration name can also be specified using `sudo nixos-rebuild switch --flake /path/to/flakes/directory#<name>`.
+        # The `nixpkgs.lib.nixosSystem` function is used to build this configuration, the following attribute set is its parameter.
+        # Run `sudo nixos-rebuild switch --flake .#nixos-test` in the flake's directory to deploy this configuration on any NixOS system
+        trinity = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
 
-        # The Nix module system can modularize configuration, improving the maintainability of configuration.
-        #
-        # Each parameter in the `modules` is a Nix Module, and there is a partial introduction to it in the nixpkgs manual:
-        #    <https://nixos.org/manual/nixpkgs/unstable/#module-system-introduction>
-        # It is said to be partial because the documentation is not complete, only some simple introductions
-        #    (such is the current state of Nix documentation...)
-        # A Nix Module can be an attribute set, or a function that returns an attribute set.
-        # If a Module is a function, this function can only have the following parameters:
-        #
-        #  lib:     the nixpkgs function library, which provides many useful functions for operating Nix expressions
-        #            https://nixos.org/manual/nixpkgs/stable/#id-1.4
-        #  config:  all config options of the current flake
-        #  options: all options defined in all NixOS Modules in the current flake
-        #  pkgs:   a collection of all packages defined in nixpkgs.
-        #           you can assume its default value is `nixpkgs.legacyPackages."${system}"` for now.
-        #           can be customed by `nixpkgs.pkgs` option
-        #  modulesPath: the default path of nixpkgs's builtin modules folder,
-        #               used to import some extra modules from nixpkgs.
-        #               this parameter is rarely used, you can ignore it for now.
-        #
-        # Only these parameters can be passed by default.
-        # If you need to pass other parameters, you must use `specialArgs` by uncomment the following line
-        specialArgs = { inherit inputs outputs; };  # pass custom arguments into sub module.
-        modules = [
-          ./hosts/trinity
-          { programs.hyprland.enable = true; }
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs; };
-            home-manager.users.orlando = import ./home/trinity;
-          }
-        ];
-      };
-      neo = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+          # The Nix module system can modularize configuration, improving the maintainability of configuration.
+          #
+          # Each parameter in the `modules` is a Nix Module, and there is a partial introduction to it in the nixpkgs manual:
+          #    <https://nixos.org/manual/nixpkgs/unstable/#module-system-introduction>
+          # It is said to be partial because the documentation is not complete, only some simple introductions
+          #    (such is the current state of Nix documentation...)
+          # A Nix Module can be an attribute set, or a function that returns an attribute set.
+          # If a Module is a function, this function can only have the following parameters:
+          #
+          #  lib:     the nixpkgs function library, which provides many useful functions for operating Nix expressions
+          #            https://nixos.org/manual/nixpkgs/stable/#id-1.4
+          #  config:  all config options of the current flake
+          #  options: all options defined in all NixOS Modules in the current flake
+          #  pkgs:   a collection of all packages defined in nixpkgs.
+          #           you can assume its default value is `nixpkgs.legacyPackages."${system}"` for now.
+          #           can be customed by `nixpkgs.pkgs` option
+          #  modulesPath: the default path of nixpkgs's builtin modules folder,
+          #               used to import some extra modules from nixpkgs.
+          #               this parameter is rarely used, you can ignore it for now.
+          #
+          # Only these parameters can be passed by default.
+          # If you need to pass other parameters, you must use `specialArgs` by uncomment the following line
+          specialArgs = { inherit inputs outputs; }; # pass custom arguments into sub module.
+          modules = [
+            ./hosts/trinity
+            { programs.hyprland.enable = true; }
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit inputs; };
+              home-manager.users.orlando = import ./home/trinity;
+            }
+          ];
+        };
+        neo = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
 
-        # The Nix module system can modularize configuration, improving the maintainability of configuration.
-        #
-        # Each parameter in the `modules` is a Nix Module, and there is a partial introduction to it in the nixpkgs manual:
-        #    <https://nixos.org/manual/nixpkgs/unstable/#module-system-introduction>
-        # It is said to be partial because the documentation is not complete, only some simple introductions
-        #    (such is the current state of Nix documentation...)
-        # A Nix Module can be an attribute set, or a function that returns an attribute set.
-        # If a Module is a function, this function can only have the following parameters:
-        #
-        #  lib:     the nixpkgs function library, which provides many useful functions for operating Nix expressions
-        #            https://nixos.org/manual/nixpkgs/stable/#id-1.4
-        #  config:  all config options of the current flake
-        #  options: all options defined in all NixOS Modules in the current flake
-        #  pkgs:   a collection of all packages defined in nixpkgs.
-        #           you can assume its default value is `nixpkgs.legacyPackages."${system}"` for now.
-        #           can be customed by `nixpkgs.pkgs` option
-        #  modulesPath: the default path of nixpkgs's builtin modules folder,
-        #               used to import some extra modules from nixpkgs.
-        #               this parameter is rarely used, you can ignore it for now.
-        #
-        # Only these parameters can be passed by default.
-        # If you need to pass other parameters, you must use `specialArgs` by uncomment the following line
-        # specialArgs = {...}  # pass custom arguments into sub module.
-        modules = [
-          ./hosts/neo
-          { programs.hyprland.enable = true; }
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs; };
-            home-manager.users.orlando = import ./home/neo;
-          }
-        ];
-      };
+          # The Nix module system can modularize configuration, improving the maintainability of configuration.
+          #
+          # Each parameter in the `modules` is a Nix Module, and there is a partial introduction to it in the nixpkgs manual:
+          #    <https://nixos.org/manual/nixpkgs/unstable/#module-system-introduction>
+          # It is said to be partial because the documentation is not complete, only some simple introductions
+          #    (such is the current state of Nix documentation...)
+          # A Nix Module can be an attribute set, or a function that returns an attribute set.
+          # If a Module is a function, this function can only have the following parameters:
+          #
+          #  lib:     the nixpkgs function library, which provides many useful functions for operating Nix expressions
+          #            https://nixos.org/manual/nixpkgs/stable/#id-1.4
+          #  config:  all config options of the current flake
+          #  options: all options defined in all NixOS Modules in the current flake
+          #  pkgs:   a collection of all packages defined in nixpkgs.
+          #           you can assume its default value is `nixpkgs.legacyPackages."${system}"` for now.
+          #           can be customed by `nixpkgs.pkgs` option
+          #  modulesPath: the default path of nixpkgs's builtin modules folder,
+          #               used to import some extra modules from nixpkgs.
+          #               this parameter is rarely used, you can ignore it for now.
+          #
+          # Only these parameters can be passed by default.
+          # If you need to pass other parameters, you must use `specialArgs` by uncomment the following line
+          # specialArgs = {...}  # pass custom arguments into sub module.
+          modules = [
+            ./hosts/neo
+            { programs.hyprland.enable = true; }
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit inputs; };
+              home-manager.users.orlando = import ./home/neo;
+            }
+          ];
+        };
 
-      weasel = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          { nix.registry.nixpkgs.flake = nixpkgs; }
-          ./hosts/weasel
-          NixOS-WSL.nixosModules.wsl
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs; };
-            home-manager.users.orlando = import ./home/weasel;
-          }
-        ];
+        weasel = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            { nix.registry.nixpkgs.flake = nixpkgs; }
+            ./hosts/weasel
+            NixOS-WSL.nixosModules.wsl
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit inputs; };
+              home-manager.users.orlando = import ./home/weasel;
+            }
+          ];
+        };
       };
     };
-  };
 }
