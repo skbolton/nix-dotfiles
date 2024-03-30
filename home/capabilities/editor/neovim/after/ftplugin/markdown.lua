695e8dd5714a0c332022d7a33975705621719485
Stephen Bolton <stephen@bitsonthemind.com>
2024-03-30 12:24:16 -0400

    bolster checkbox toggling in markdown files
    Copied all of this functionality from
    https://github.com/opdavies/toggle-checkbox.nvim to avoid adding another
    dependency

--- home/capabilities/editor/neovim/after/ftplugin/markdown.lua
+++ home/capabilities/editor/neovim/after/ftplugin/markdown.lua
@@ -66,20 +66,71 @@ blocal.shiftwidth = 2
 
 vim.g.bullets_outline_levels = {'num', 'abc', 'std*'}
 
-local toggle_todo = function()
-  local linenr = vim.api.nvim_win_get_cursor(0)[1]
-  local curline = vim.api.nvim_buf_get_lines(0, linenr - 1, linenr, false)[1]
+-- copied from https://github.com/opdavies/toggle-checkbox.nvim/blob/main/lua/toggle-checkbox.lua
+local checked_character = "x"
 
-  if string.find(curline, "%- %[ ]") then
-    vim.cmd('s/\\v-\\s\\[\\s\\]/- [x]')
-    vim.cmd('nohlsearch')
-  elseif string.find(curline, "%- %[x]") then
-    vim.cmd('s/\\v-\\s\\[x\\]/- [ ]')
-    vim.cmd('nohlsearch')
-  end
+local checked_checkbox = "%[" .. checked_character .. "%]"
+local unchecked_checkbox = "%[ %]"
+
+local line_contains_unchecked = function(line)
+  return line:find(unchecked_checkbox)
 end
 
-vim.keymap.set('n', '<C-SPACE>', toggle_todo, { buffer = true })
+local line_contains_checked = function(line)
+  return line:find(checked_checkbox)
+end
+
+local line_with_checkbox = function(line)
+  -- return not line_contains_a_checked_checkbox(line) and not line_contains_an_unchecked_checkbox(line)
+  return line:find("^%s*- " .. checked_checkbox)
+    or line:find("^%s*- " .. unchecked_checkbox)
+    or line:find("^%s*%d%. " .. checked_checkbox)
+    or line:find("^%s*%d%. " .. unchecked_checkbox)
+end
+
+local checkbox = {
+  check = function(line)
+    return line:gsub(unchecked_checkbox, checked_checkbox, 1)
+  end,
+
+  uncheck = function(line)
+    return line:gsub(checked_checkbox, unchecked_checkbox, 1)
+  end,
+
+  make_checkbox = function(line)
+    if not line:match("^%s*-%s.*$") and not line:match("^%s*%d%s.*$") then
+      -- "xxx" -> "- [ ] xxx"
+      return line:gsub("(%S+)", "- [ ] %1", 1)
+    else
+      -- "- xxx" -> "- [ ] xxx", "3. xxx" -> "3. [ ] xxx"
+      return line:gsub("(%s*- )(.*)", "%1[ ] %2", 1):gsub("(%s*%d%. )(.*)", "%1[ ] %2", 1)
+    end
+  end,
+}
+
+local toggle_checkbox = function()
+  local bufnr = vim.api.nvim_buf_get_number(0)
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local start_line = cursor[1] - 1
+  local current_line = vim.api.nvim_buf_get_lines(bufnr, start_line, start_line + 1, false)[1] or ""
+
+  -- If the line contains a checked checkbox then uncheck it.
+  -- Otherwise, if it contains an unchecked checkbox, check it.
+  local new_line = ""
+
+  if not line_with_checkbox(current_line) then
+    new_line = checkbox.make_checkbox(current_line)
+  elseif line_contains_unchecked(current_line) then
+    new_line = checkbox.check(current_line)
+  elseif line_contains_checked(current_line) then
+    new_line = checkbox.uncheck(current_line)
+  end
+
+  vim.api.nvim_buf_set_lines(bufnr, start_line, start_line + 1, false, { new_line })
+  vim.api.nvim_win_set_cursor(0, cursor)
+end
+
+vim.keymap.set('n', '<c-space>', toggle_checkbox)
 
 vim.keymap.set("n", "]t", function()
   require("todo-comments").jump_next()
