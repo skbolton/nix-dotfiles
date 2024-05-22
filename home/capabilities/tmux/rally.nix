9dca28fd9b53fa22e04a8e6ddd87b749fd02aa83
Stephen Bolton <stephen@bitsonthemind.com>
2024-05-22 04:45:11 +0000

    Remove git folder from rally
    It was causing some problems and not fulfilling what I wanted from it.

--- home/capabilities/tmux/rally.nix
+++ home/capabilities/tmux/rally.nix
@@ -2,7 +2,7 @@
 
 pkgs.writeShellScriptBin "rally.sh" ''
   set -eu
-  TARGET=$(ls -d ~/Public/* ~/* /tmp/git/* | ${pkgs.fzf}/bin/fzf --header-first --header="Launch Project" --prompt="🔮 " --preview '${pkgs.eza}/bin/eza --tree --icons --color=always --level 3 --git-ignore {}')
+  TARGET=$(ls -d ~/Public/* ~/* | ${pkgs.fzf}/bin/fzf --header-first --header="Launch Project" --prompt="🔮 " --preview '${pkgs.eza}/bin/eza --tree --icons --color=always --level 3 --git-ignore {}')
   NAME=$(basename $TARGET)
   SESSION_NAME=$(echo $NAME | tr [:lower:] [:upper:])
 
