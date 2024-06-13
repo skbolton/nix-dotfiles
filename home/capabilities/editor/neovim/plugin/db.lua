4d2549e31535da80d6fd4481274080b193d876f5
Stephen Bolton <stephen@bitsonthemind.com>
2024-06-13 06:19:55 +0000

    Drop notify support from dadbod
    Something that was missed when I got rid of notify

--- home/capabilities/editor/neovim/plugin/db.lua
+++ home/capabilities/editor/neovim/plugin/db.lua
@@ -1,6 +1,6 @@
 vim.g.db_ui_use_nerd_fonts = true
 vim.g.db_ui_execute_on_save = false
 vim.g.db_ui_save_location = "./steve_queries"
-vim.g.db_ui_use_nvim_notify = true
+vim.g.db_ui_use_nvim_notify = false
 vim.g.db_ui_disable_mappings = true
 vim.g.db_url = os.getenv("DBUI_URL")
