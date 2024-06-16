92f30cb72af561e509cd38d4fe920464a0f6c21b
Stephen Bolton <stephen@bitsonthemind.com>
2024-06-16 13:35:07 -0400

    Add light program to neo
    This is doing the trick to get the backlight hotkeys to work. Before
    acpi errors were flooding the journal when pressing the keys.

--- hosts/neo/default.nix
+++ hosts/neo/default.nix
@@ -154,6 +154,12 @@
     cue = true;
   };
 
+  programs.light = {
+    enable = true;
+    brightnessKeys.step = 5;
+    brightnessKeys.enable = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
