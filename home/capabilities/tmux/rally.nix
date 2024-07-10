9f3339b7764f5224e8f0cf86b61f6a2a5e31adc8
Stephen Bolton <stephen@bitsonthemind.com>
2024-07-10 19:03:06 -0400

    Auto attach to project smug templates

--- home/capabilities/tmux/rally.nix
+++ home/capabilities/tmux/rally.nix
@@ -6,12 +6,12 @@
 
 writeShellScriptBin "rally.sh" ''
   set -eu
-  TARGET=$(ls -d ~/Public/* ~/* | ${fzf}/bin/fzf --header-first --header="Launch Project" --prompt="🔮 " --preview '${eza}/bin/eza --tree --icons --color=always --level 3 --git-ignore {}')
+  TARGET=$(ls -d ~/Public/* ~/* | ${fzf}/bin/fzf --header-first --header="Launch Project" --prompt="🔮 " --preview '${eza}/bin/eza --tree --icons --level 3 --git-ignore {}' --delimiter '/' --with-nth -2..-1)
   NAME=$(basename $TARGET)
   SESSION_NAME=$(echo $NAME | tr [:lower:] [:upper:])
 
   if [[ -f "$TARGET/.steve-smug.yml" ]]; then
-    ${smug}/bin/smug start -f "$TARGET/.steve-smug.yml"
+    ${smug}/bin/smug start -f "$TARGET/.steve-smug.yml" -a
   elif [[ -f "$HOME/.config/smug/$NAME.yml" ]]; then
     ${smug}/bin/smug start $NAME -a
   else
