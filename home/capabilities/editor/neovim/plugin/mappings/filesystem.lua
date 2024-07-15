98ac9be1a24b44ae4410cb9c0483390b17287ef6
Stephen Bolton <stephen@bitsonthemind.com>
2024-07-15 10:04:05 -0400

    Add more oil options
    Things like show hidden files and use trash cli

--- home/capabilities/editor/neovim/plugin/mappings/filesystem.lua
+++ home/capabilities/editor/neovim/plugin/mappings/filesystem.lua
@@ -2,6 +2,11 @@ local map = vim.keymap.set
 local oil = require 'oil'
 
 oil.setup {
+  default_file_explorer = true,
+  columns = {
+    "icon",
+    "permissions",
+  },
   keymaps = {
     -- stub out defaults that clash
     ["<C-s>"] = false,
@@ -9,6 +14,14 @@ oil.setup {
     ["<C-l>"] = false,
     ["<C-x>"] = "actions.select_split",
     ["<C-v>"] = "actions.select_vsplit",
+  },
+  delete_to_trash = true,
+  skip_confirm_for_simple_edits = true,
+  view_options = {
+    show_hidden = true,
+    is_always_hidden = function(name)
+      return name == ".." or name == ".git"
+    end
   }
 }
 
