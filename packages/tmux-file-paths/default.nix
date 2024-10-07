e1b3b80b386acbce9d507f840c0b7da3a47dd0df
Stephen Bolton <stephen@bitsonthemind.com>
2024-10-06 22:44:14 -0400

    Refine tmux-file-paths
    Harden argument parsing and option to pass an additional filter to found
    paths.

--- packages/tmux-file-paths/default.nix
+++ packages/tmux-file-paths/default.nix
@@ -5,19 +5,54 @@
 }:
 
 writeShellScriptBin "tmux-file-paths" ''
-  pane_file=$HOME/.cache/tmux-find-file-paths
-  truncate -s 0 $pane_file
+  POSITIONAL_ARGS=()
+  PREVIEWER="print"
+  FILEPATHREGEX='(([.\w\\-~\$@]+)?(/[.\w\-@]+)+\.\w+/?(:\d+))'
 
-  for id in $(tmux list-panes -s -F '#D'); do
-    tmux capture -p -t $id >> $pane_file
+  # Empty/create file to hold pane contents
+  PANEFILE=$HOME/.cache/tmux-find-file-paths
+  truncate -s 0 $PANEFILE
+
+  # Procuss arguments
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      -f|--fuzzy)
+      PREVIEWER="fuzzy"
+      # PREVIEWER="${fzf}/bin/fzf --delimiter ':' --preview '${bat}/bin/bat {1} --color=always --highlight-line {2} --line-range {2}:'"
+      shift
+      ;;
+      -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+    POSITIONAL_ARGS+=("$1")
+    shift
+    esac
   done
 
-  results=$(${ripgrep}/bin/rg --color=never --only-matching '(([.\w\\-~\$@]+)?(/[.\w\-@]+)+\.\w+/?(:\d+))' $pane_file)
+  # restore positional parameters
+  set -- "''${POSITIONAL_ARGS[@]}"
 
-  if [[ $1 == "-f" ]]; then
-    ${ripgrep}/bin/rg --no-line-number --color=never --only-matching '(([.\w\\-~\$@]+)?(/[.\w\-@]+)+\.\w+/?(:\d+))' $pane_file | sort -u | ${fzf}/bin/fzf --delimiter ':' --preview '${bat}/bin/bat {1} --color=always --highlight-line {2} --line-range {2}:'
-  else
-    ${ripgrep}/bin/rg --no-line-number --color=never --only-matching '(([.\w\\-~\$@]+)?(/[.\w\-@]+)+\.\w+/?(:\d+))' $pane_file | sort -u
-  fi
+  DEFAULT_FILTER=".*"
+  ADDITIONAL_FILTER="''${1:-$DEFAULT_FILTER}"
+
+  # Capture all panes into panefile
+  for id in $(tmux list-panes -s -F '#D'); do
+    tmux capture -p -t $id >> $PANEFILE
+  done
+
+  preview() {
+    if [[ $PREVIEWER = "fuzzy" ]]; then
+      ${fzf}/bin/fzf --delimiter ':' --preview '${bat}/bin/bat {1} --color=always --highlight-line {2} --line-range {2}:'
+    else
+      xargs -0 echo
+    fi
+  }
+
+  ${ripgrep}/bin/rg --no-line-number --only-matching $FILEPATHREGEX $PANEFILE \
+    | sort -u \
+    | ${ripgrep}/bin/rg --color=never --null "$ADDITIONAL_FILTER" \
+    | preview
 ''
 
