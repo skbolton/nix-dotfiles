76653f49d2c16453011c97b7f34a6a7f566f66fd
Stephen Bolton <stephen@bitsonthemind.com>
2024-12-16 13:04:37 -0500

    Missed zk lazy load file

--- modules/home/neovim/neovim/plugin/notes.lua
+++ modules/home/neovim/neovim/plugin/notes.lua
@@ -1,7 +1,3 @@
-local zk = require('zk')
-local util = require('zk.util')
-local commands = require('zk.commands')
-
 require("neorg").setup {
   load = {
     ["core.defaults"] = {},
@@ -32,6 +28,7 @@ require("neorg").setup {
         engine = "nvim-cmp"
       }
     },
+    ["core.promo"] = {},
     ["core.concealer"] = {
       config = {
         icon_preset = "diamond",
@@ -53,56 +50,5 @@ require("neorg").setup {
     }
   }
 }
-
-zk.setup {
-  picker = "telescope",
-  lsp = {
-    auto_attach = {
-      enabled = true
-    }
-  }
-}
-
-commands.add("ZkFromSelection", function(options)
-  vim.ui.input({ prompt = "Title: " }, function(input)
-    local location = util.get_lsp_location_from_selection()
-    local selected_text = util.get_text_in_range(location.range)
-    assert(selected_text ~= nil, "No selected text")
-
-    options = options or {}
-    options.content = selected_text
-
-    if options.inline == true then
-      options.inline = nil
-      options.dryRun = true
-      options.insertContentAtLocation = location
-    else
-      options.insertLinkAtLocation = location
-    end
-
-    zk.new(vim.tbl_extend("force", { title = input }, options))
-  end)
-end, { needs_selection = true })
-
-commands.add("ZkProjects", function(options)
-  options = options or {}
-  local tags = options.tags or {}
-  tags[#tags + 1] = "PROJECT"
-  tags[#tags + 1] = "open"
-  options = vim.tbl_extend("force", { tags = tags }, options)
-  zk.edit(options, { title = "Open Projects" })
-end)
-
-commands.add("ZkSpells", function(options)
-  options = vim.tbl_extend("force", { tags = { "SPELL" } }, options or {})
-  zk.edit(options, { title = "Spellbook" })
-end)
-
-vim.keymap.set('v', '<leader>ne', ':ZkFromSelection<CR>')
-vim.keymap.set('n', '<leader>ne', function()
-  vim.ui.input({ prompt = "Title: " }, function(input)
-    vim.cmd("ZkNew { title = '" .. input .. "'}")
-  end)
-end)
 
 vim.keymap.set('n', '<leader>n^', ':e Logs/<C-R>=strftime("%Y-%m-%d")<CR>.md<CR>', { buffer = true, silent = true })
