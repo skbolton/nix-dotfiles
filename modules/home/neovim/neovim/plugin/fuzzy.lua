d4b0e68fe5f448fecab54dff6b474657fb490aa4
Stephen Bolton <stephen@bitsonthemind.com>
2024-12-16 12:57:11 -0500

    Lazy load zk

--- modules/home/neovim/neovim/plugin/fuzzy.lua
+++
@@ -1,9 +0,0 @@
-local wk = require 'which-key'
-
-wk.add {
-  { "<leader>nn",    "<CMD>ZkNotes<CR>",                  desc = "Find note",      group = "+notes" },
-  { "<leader>nN",    ":ZkNotes { tags = {}}<left><left>", desc = "Notes with tag", group = "+notes" },
-  { "<leader>nt",    "<CMD>ZkTags<CR>",                   desc = "Tag search",     group = "+notes" },
-  { "<leader>n.",    "<CMD>ZkBacklinks<CR>",              desc = "Backlinks",      group = "+notes" },
-  { "<leader>n<up>", "<CMD>ZkLinks<CR>",                  desc = "Outbound links", group = "+notes" },
-}
