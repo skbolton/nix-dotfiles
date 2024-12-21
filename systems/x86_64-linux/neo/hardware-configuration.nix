cb949319d7c6a6e63cdbad94aec104bcb38961bc
Stephen Bolton <stephen@bitsonthemind.com>
2024-12-21 14:35:54 -0500

    Fix copilot key on neo
    Using keyd allows me to remap the left shift left meta combination that
    copilot sends to make it send control.

--- systems/x86_64-linux/neo/hardware-configuration.nix
+++ systems/x86_64-linux/neo/hardware-configuration.nix
@@ -63,6 +63,16 @@
 
   swapDevices = [{ device = "/swap/swapfile"; size = 24 * 1024; }];
 
+  services.keyd = {
+    enable = true;
+    keyboards.detault.settings = {
+      main = {
+        "leftshift+leftmeta" = "rightcontrol";
+      };
+    };
+  };
+
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
