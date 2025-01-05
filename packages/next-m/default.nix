119b3638145b7cf69c1aa0c023845ce7696142c3
Stephen Bolton <stephen@bitsonthemind.com>
2025-01-05 03:28:56 -0500

    Set day 1 of planning system to January 5th 2025
    No more day 1's after this....

--- packages/next-m/default.nix
+++ packages/next-m/default.nix
@@ -2,7 +2,7 @@
 
 writeShellScriptBin "nextm" ''
   FROM=''${1:-$(date --iso-8601=date)}
-  START=2024-12-29
+  START=2025-01-05
 
   DIFF=$(($(${dateutils}/bin/datediff $START $FROM -f '%d') % 28))
   ${dateutils}/bin/dateadd $FROM $((28 - $DIFF))d
