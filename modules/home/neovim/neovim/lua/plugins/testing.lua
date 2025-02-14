4eb66382a4dbfb00fb2f9dd3951095a43c6ab0ee
Stephen Bolton <stephen@bitsonthemind.com>
2025-02-14 01:10:42 -0500

    Move testing keybinds to keys table

--- modules/home/neovim/neovim/lua/plugins/testing.lua
+++ modules/home/neovim/neovim/lua/plugins/testing.lua
@@ -25,17 +25,15 @@ return {
 
       g.dispatch_compilers = { elixir = 'exunit' }
     end,
-    after = function()
-      local map = vim.keymap.set
-
-      map('n', '<leader>tT', '<CMD>TestFile<CR>')
-      map('n', '<leader>tt', '<CMD>TestFile -strategy=vimux_watch<CR>')
-      map('n', '<leader>tN', '<CMD>TestNearest<CR>')
-      map('n', '<leader>tn', '<CMD>TestNearest -strategy=vimux_watch<CR>')
-      map('n', '<leader>t.', '<CMD>TestLast<CR>')
-      map('n', '<leader>tv', '<CMD>TestVisit<CR>zz')
-      map('n', '<leader>ts', '<CMD>TestSuite -strategy=vimux_watch<CR>')
-      map('n', '<leader>tS', '<CMD>TestSuite<CR>')
-    end
+    keys = {
+      { '<leader>tT', '<CMD>TestFile<CR>' },
+      { '<leader>tt', '<CMD>TestFile -strategy=vimux_watch<CR>' },
+      { '<leader>tN', '<CMD>TestNearest<CR>' },
+      { '<leader>tn', '<CMD>TestNearest -strategy=vimux_watch<CR>' },
+      { '<leader>t.', '<CMD>TestLast<CR>' },
+      { '<leader>tv', '<CMD>TestVisit<CR>zz' },
+      { '<leader>ts', '<CMD>TestSuite -strategy=vimux_watch<CR>' },
+      { '<leader>tS', '<CMD>TestSuite<CR>' }
+    }
   }
 }
