800ebf7e5fe4df5667bd918cf8ebe01d2b2847b4
Stephen Bolton <stephen@bitsonthemind.com>
2025-04-02 12:53:16 -0400

    Add missing visual keybinds in sql files

--- modules/home/neovim/neovim/after/ftplugin/sql.lua
+++ modules/home/neovim/neovim/after/ftplugin/sql.lua
@@ -1,8 +1,9 @@
 local keymap = vim.keymap
 
 keymap.set('n', '<leader>\\', '<CMD>DBUIToggle<CR>', { buffer = true })
--- visually select the area under the cursor and run the query
 keymap.set('n', '<localleader>r', ':normal vip<CR><PLUG>(DBUI_ExecuteQuery)', { buffer = true })
+keymap.set('v', '<localleader>r', '<PLUG>(DBUI_ExecuteQuery)<CR>', { buffer = true })
 keymap.set('n', '<localleader>F', ':%!sqlFormat .<CR>', { buffer = true })
 keymap.set('n', '<localleader>f', ':normal vip<CR>:!sqlFormat<CR>', { buffer = true })
+keymap.set('v', '<localleader>f', ':!sqlFormat<CR>', { buffer = true })
 keymap.set('n', '<leader>w', '<PLUG>(DBUI_SaveQuery)', { buffer = true })
