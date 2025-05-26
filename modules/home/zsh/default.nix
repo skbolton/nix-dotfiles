2f30e393d9e41728c62345a03e392c5a4f48ccd4
Stephen Bolton <stephen@bitsonthemind.com>
2025-05-26 18:54:52 -0400

    Customize fzf tab behavior

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -74,6 +74,27 @@ in
           setopt CDABLE_VARS          # Change directory to a path stored in a variable
           setopt EXTENDED_GLOB        # Use extended globbing syntax
           KEYTIMEOUT=5
+
+          # use tmux popup
+          zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+          # popup minimum size
+          zstyle ':fzf-tab:*' popup-min-size 50 8
+          # disable sort when completing `git checkout`
+          zstyle ':completion:*:git-checkout:*' sort false
+          # set descriptions format to enable group support
+          # NOTE: don't use escape sequences (like '%F{red}%d%f') here, fzf-tab will ignore them
+          zstyle ':completion:*:descriptions' format '[%d]'
+          # set list-colors to enable filename colorizing
+          zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+          # force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+          zstyle ':completion:*' menu no
+          # preview directory's content with eza when completing cd
+          zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+          # To make fzf-tab follow FZF_DEFAULT_OPTS.
+          # NOTE: This may lead to unexpected behavior since some flags break this plugin. See Aloxaf/fzf-tab#455.
+          zstyle ':fzf-tab:*' use-fzf-default-opts yes
+          # switch group using `<` and `>`
+          zstyle ':fzf-tab:*' switch-group '<' '>'
         '')
         (mkOrder 1000 ''
           autoload -Uz edit-command-line
