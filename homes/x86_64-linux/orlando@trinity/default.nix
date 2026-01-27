4857a1db4fb4de20f4146e1584b226fa0a1bf7b6
Stephen Bolton <stephen@bitsonthemind.com>
2026-01-27 01:45:29 -0500

    Remove the niobe package
    I can now achieve what I want with the ssh kitten that kitty provides

--- homes/x86_64-linux/orlando@trinity/default.nix
+++ homes/x86_64-linux/orlando@trinity/default.nix
@@ -108,7 +108,6 @@
       pkgs.delta.fman
       handbrake
       delta.quantified-self
-      delta.niobe
     ];
   };
 
--- packages/niobe/default.nix
+++
@@ -1,9 +0,0 @@
-{ makeDesktopItem, ... }:
-
-makeDesktopItem {
-  name = "Kitty - Weasel";
-  desktopName = "niobe";
-  icon = ./kitty-light.png;
-  exec = "kitty --config /home/orlando/.config/kitty/kitty-light.conf";
-  terminal = false;
-}
